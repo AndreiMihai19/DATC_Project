47d66242f71452ff59358afe85cb5bbe0b802137
Andrei Mihai <79441424+AndreiMihai19@users.noreply.github.com>
2024-01-08 20:40:40 +0200

    Add files via upload

---
+++ HomematicWeb/HomematicApp.Context/Context/HomematicContext.cs
@@ -0,0 +1,26 @@
+﻿using HomematicApp.Context.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Action = HomematicApp.Context.DbModels.Action;
+
+namespace HomematicApp.Context.Context
+{
+    public class HomematicContext : DbContext
+    {
+        public HomematicContext(DbContextOptions<HomematicContext> options)
+        : base(options)
+        { }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Action> actions { get; set; }
+        public DbSet<Parameter> parameters { get; set; }
+        public DbSet<Preset> presets { get; set; }
+        public DbSet<EspData> temperature_ESP { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Action>()
+				.Property(e => e.Action_Type)
+				.HasConversion<string>();
+		}
+	}
+}
