85945c86f9ffe719b65468c189d29ea38133296f
Andrei Mihai <79441424+AndreiMihai19@users.noreply.github.com>
2024-01-08 20:43:34 +0200

    Add files via upload

---
+++ HomematicWeb/HomematicApp.Domain/Abstractions/IAuthenticationRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using HomematicApp.Context.DbModels;
+
+namespace HomematicApp.Domain.Abstractions
+{
+    public interface IAuthenticationRepository
+    {
+        Task<bool> Register(User user);
+        Task<string?> Login(LoginUser loginUser);
+        Task<bool> ForgotPassword(string email, string link);
+        Task<bool> ResetPassword(string email, string newPassword);
+        Task<bool> ConfirmResetPasswordRequest(string email, string token, string cookieToken);
+        Task<string> GetRole(string email);
+    }
+}
